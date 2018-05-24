d9c25ef2d63f0e6338af61a7d0be9f507d233b9c
Luis Felipe Tapajos <lftapajos@gmail.com>
2018-05-24 18:57:28 -0300

    dependencies created

---
+++ wallet_stone/wallet_stone/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  wallet_stone
+//
+//  Created by Luis Felipe Tapajos on 24/05/18.
+//  Copyright © 2018 Luis Felipe Tapajos. All rights reserved.
+//
+
+import Foundation
